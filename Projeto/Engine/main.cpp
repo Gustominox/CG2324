28fbd5e429a6dcf8e50cab7b364354c340e73dcb
Gustominox <agstcmps.111@gmail.com>
2024-04-26 14:43:53 +0100

    Adicionar codigo do alex

--- Projeto/Engine/main.cpp
+++ Projeto/Engine/main.cpp
@@ -37,6 +37,74 @@ float mode = GL_LINE;
 int timebase = glutGet(GLUT_ELAPSED_TIME);
 int frame = 0;
 
+void buildRotMatrix(float *x, float *y, float *z, float *m)
+{
+
+	m[0] = x[0];
+	m[1] = x[1];
+	m[2] = x[2];
+	m[3] = 0;
+	m[4] = y[0];
+	m[5] = y[1];
+	m[6] = y[2];
+	m[7] = 0;
+	m[8] = z[0];
+	m[9] = z[1];
+	m[10] = z[2];
+	m[11] = 0;
+	m[12] = 0;
+	m[13] = 0;
+	m[14] = 0;
+	m[15] = 1;
+}
+
+void cross(float *a, float *b, float *res)
+{
+
+	res[0] = a[1] * b[2] - a[2] * b[1];
+	res[1] = a[2] * b[0] - a[0] * b[2];
+	res[2] = a[0] * b[1] - a[1] * b[0];
+}
+
+void normalize(float *a)
+{
+
+	float l = sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
+	a[0] = a[0] / l;
+	a[1] = a[1] / l;
+	a[2] = a[2] / l;
+}
+
+float length(float *v)
+{
+
+	float res = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+	return res;
+}
+
+void multMatrixVector(float *m, float *v, float *res)
+{
+
+	for (int j = 0; j < 4; ++j)
+	{
+		res[j] = 0;
+		for (int k = 0; k < 4; ++k)
+		{
+			res[j] += v[k] * m[j * 4 + k];
+		}
+	}
+}
+
+float multVectors(float *m, float *v)
+{
+	float res = 0;
+	for (int i = 0; i < 4; ++i)
+	{
+		res = res + m[i] * v[i];
+	}
+	return res;
+}
+
 class Point
 {
 private:
@@ -94,15 +162,136 @@ public:
 		points.push_back(point);
 	}
 
-	void apply() const
+	void getCatmullRomPoint(float t, Point p0, Point p1, Point p2, Point p3, Point *pos, Point *deriv)
+	{
+
+		// catmull-rom matrix
+		float m[4][4] = {{-0.5f, 1.5f, -1.5f, 0.5f},
+						 {1.0f, -2.5f, 2.0f, -0.5f},
+						 {-0.5f, 0.0f, 0.5f, 0.0f},
+						 {0.0f, 1.0f, 0.0f, 0.0f}};
+
+		// Compute A = M * P
+		float vecX[4] = {p0.getX(), p1.getX(), p2.getX(), p3.getX()}; // x
+		float vecY[4] = {p0.getY(), p1.getY(), p2.getY(), p3.getY()}; // y
+		float vecZ[4] = {p0.getZ(), p1.getZ(), p2.getZ(), p3.getZ()}; // z
+
+		float A[3][4];
+		multMatrixVector(*m, vecX, A[0]);
+		multMatrixVector(*m, vecY, A[1]);
+		multMatrixVector(*m, vecZ, A[2]);
+
+		// Compute pos = T * A
+
+		float tv[4] = {t * t * t, t * t, t, 1};
+
+		(*pos).setX(multVectors(A[0], tv));
+		(*pos).setY(multVectors(A[1], tv));
+		(*pos).setZ(multVectors(A[2], tv));
+
+		//		std:: cout <<"valores de p:"<<pos.getX()<<", "<< pos.getY()<<", " <<pos.getZ() <<std::endl;
+
+		// compute deriv = T' * A
+
+		float tvd[4] = {3 * t * t, 2 * t, 1, 0};
+		Point derivV;
+		(*deriv).setX(multVectors(A[0], tvd));
+		(*deriv).setY(multVectors(A[1], tvd));
+		(*deriv).setZ(multVectors(A[2], tvd));
+
+		// ...
+	}
+
+	// given  global t, returns the point in the curve
+	void getGlobalCatmullRomPoint(float gt, Point *pos, Point *deriv)
+	{
+		int point_count = points.size();
+		float t = gt * point_count; // this is the real global t
+		int index = floor(t);		// which segment
+		t = t - index;				// where within  the segment
+
+		// indices store the points
+		int indices[4];
+		indices[0] = (index + point_count - 1) % point_count;
+		indices[1] = (indices[0] + 1) % point_count;
+		indices[2] = (indices[1] + 1) % point_count;
+		indices[3] = (indices[2] + 1) % point_count;
+
+		getCatmullRomPoint(t, points[indices[0]], points[indices[1]],
+						   points[indices[2]], points[indices[3]], pos, deriv);
+	}
+
+	void renderCatmullRomCurve(Point pos, Point deriv)
+	{
+
+		// draw curve using line segments with GL_LINE_LOOP
+		float gt = 0.0;
+		int NUM_STEPS = 1000;
+		float step = 1.0 / NUM_STEPS;
+		glBegin(GL_LINE_LOOP);
+		for (int i = 0; i < NUM_STEPS; i++)
+		{
+			getGlobalCatmullRomPoint(gt, &pos, &deriv);
+			std::cout << "valores de p:" << pos.getX() << ", " << pos.getY() << ", " << pos.getZ() << std::endl;
+
+			glVertex3f(pos.getX(), pos.getY(), pos.getZ());
+
+			gt += step;
+		}
+		glEnd();
+	}
+
+	void apply()
 	{
 		if (type == "translate")
 		{
-			glTranslatef(x, y, z);
+			if (points.size() == 0)
+			{
+				glTranslatef(x, y, z);
+			}
+			else
+			{
+				float static t = 0;
+				Point pos1;
+				Point deriv1;
+				float elapse = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
+				float gt = elapse / time;
+				renderCatmullRomCurve(pos1, deriv1);
+
+				// apply transformations here
+
+				Point pos2;
+				Point deriv2;
+				getGlobalCatmullRomPoint(gt, &pos2, &deriv2);
+				glTranslatef(pos2.getX(), pos2.getY(), pos2.getZ());
+
+				if (align)
+				{
+
+					float vecAux[3] = {0, 1, 0};
+					float vecX[3] = {deriv2.getX(), deriv2.getY(), deriv2.getZ()};
+					normalize(vecX);
+
+					float vecZ[3];
+					cross(vecX, vecAux, vecZ);
+					normalize(vecZ);
+
+					float vecY[3];
+					cross(vecZ, vecX, vecY);
+					normalize(vecY);
+
+					float m[16];
+					buildRotMatrix(vecX, vecY, vecZ, m);
+
+					glMultMatrixf(m);
+				}
+
+				// t =+ 0.001;
+			}
 		}
 		else if (type == "rotate")
 		{
-			glRotatef(angle, x, y, z);
+			glRotatef(angle, x, y, z); // Use angle in glRotatef
 		}
 		else if (type == "scale")
 		{
@@ -517,7 +706,7 @@ void printConfig(const pugi::xml_node &node, int depth = 0)
 int main(int argc, char **argv)
 {
 
-	std::string xmlFilePath = CONFIGS_DIR + "/test_files_phase_2/" + "test_2_1.xml"; // linux com rotacoes
+	std::string xmlFilePath = CONFIGS_DIR + "/test_files_phase_3/" + "test_3_1.xml"; // linux com rotacoes
 
 	// Load the XML file
 	pugi::xml_document doc;
